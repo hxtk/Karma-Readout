9a2eaefc8c70f000844bd6ddb837e37b9ae84bfc
hxtk <edistojr+a@gmail.com>
2014-06-20 02:14:32 -0400

    modified:   data/script.js 	modified:   lib/main.js

--- Firefox/data/script.js
+++ Firefox/data/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-    var n = $('.score .number').html();
+    var n = $('.linkinfo .score .number').html();
+        n = n.replace(/\D/g,'');
 
-    var p = $('.score').html();
-        p = p.substring(p.indexOf('(')+1,p.indexOf('%');
+    var p = $('.linkinfo .score').html();
+        p = p.substring(p.indexOf('(')+1,p.indexOf('%'));
 
-    var y = (100-p)*n/(2p-101);
-    var x = n + y;
+    var y = (100-p)*n/(2*p-101);
+        y = Math.round(y);
+    var x = parseInt(n) + parseInt(y);
 
-    var text = $('.score').html();
+    var text = $('.linkinfo .score').html();
 
-    text =  text + '<span class="upvotes"><span class="number">'+
-            Math.round(x)+
-            '</span>&#32;<span class="word">upvotes</span></span>&#32;'+
-            '<span class="downvotes"><span class="number">'+
-            Math.round(y)+
-            '</span>&#32;<span class="word">downvotes</span></span>';
+    text =  text + '<br><span class="upvotes" style="color:#FF4500; font-size:80%">'+
+            x + '&#32;upvotes</span>&#32;'+
+            '<span class="downvotes" style="color:#5F99CF; font-size:80%">'+
+            y + '&#32;downvotes</span>';
 
     $('.score').html(text);
 });
