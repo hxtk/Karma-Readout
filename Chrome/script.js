6e4074e45e671e6ccfe63b4f8a7595d613f78d29
hxtk <edistojr+a@gmail.com>
2014-06-22 20:26:48 -0400

    fix formula

--- Chrome/script.js
+++ Chrome/script.js
@@ -1,18 +1,22 @@
+function comaify(x) {
+    return x.toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
+}
+
 var n = $('.linkinfo .score .number').html();
     n = n.replace(/\D/g,'');
 
 var p = $('.linkinfo .score').html();
     p = p.substring(p.indexOf('(')+1,p.indexOf('%'));
 
-var y = (100-p)*n/(2*p-101);
+var y = (100-p)*n/(2*p-100);
     y = Math.round(y);
 var x = parseInt(n) + parseInt(y);
 
 var text = $('.linkinfo .score').html();
 
 text =  text + '<br><span style="color:#FF4500; font-size:80%">'+
-        x + '&#32;upvotes</span>&#32;'+
+        comaify(x) + '&#32;upvotes</span>&#32;'+
         '<span style="color:#5F99CF; font-size:80%">'+
-        y + '&#32;downvotes</span>';
+        comaify(y) + '&#32;downvotes</span>';
 
 $('.linkinfo .score').html(text);
